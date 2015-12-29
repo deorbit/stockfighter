f0fd151a4242b55bfd01ba36f0a9e6a77d408cf8
Adam Hollidge <adam@anh.io>
2015-12-29 01:09:49 -0500

    Add Venue struct and availability check.

--- stockfighter.go
+++ stockfighter.go
@@ -2,9 +2,15 @@ package stockfighter
 
 import (
   "fmt"
+  "net/http"
+  "encoding/json"
+  "io/ioutil"
+  "bytes"
+  "os"
 )
 
 const BaseURL string = "https://api.stockfighter.io/ob/api"
+var apiKey string = os.Getenv("STOCKFIGHTER_API_KEY")
 
 type Order struct {
   Account string `json:"accounts"`
@@ -16,8 +22,49 @@ type Order struct {
   OrderType string `json:"orderType"`
 }
 
-func (o *Order)Execute()(err error) {
+func (o *Order)Execute()(error) {
   fmt.Println("Executing", o)
 
+  orderJSON, _ := json.Marshal(o)
+
+  fmt.Printf("%s", orderJSON)
+
+  req, err := http.NewRequest("POST", BaseURL, bytes.NewBuffer(orderJSON))
+  req.Header.Set("X-Starfighter-Authorization", apiKey)
+  req.Header.Set("Content-Type", "application/json")
+
+  client := &http.Client{}
+  resp, err := client.Do(req)
+  if err != nil {
+    panic(err)
+  }
+  defer resp.Body.Close()
+  fmt.Println("response Status:", resp.Status)
+  fmt.Println("response Headers:", resp.Header)
+  body, _ := ioutil.ReadAll(resp.Body)
+  fmt.Println("response Body:", string(body))
+
   return nil
+}
+
+type Venue struct {
+  Symbol string `json:"venue"`
+}
+
+func (v *Venue)Up()(bool) {
+  fmt.Println(BaseURL + "/venues/" + v.Symbol + "/heartbeat")
+  resp, err := http.Get(BaseURL + "/venues/" + v.Symbol + "/heartbeat")
+  if err != nil {
+    return false
+  }
+
+  body, _ := ioutil.ReadAll(resp.Body)
+  var dat map[string]interface{}
+  json.Unmarshal(body, &dat)
+  if (dat["ok"] != true) {
+    fmt.Println(dat["error"])
+    return false
+  }
+
+  return true
 }
